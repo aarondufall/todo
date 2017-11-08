02a14aca826fc2bf36649d5ba949b0ab2d2caded
ntl <nathanladd+github@gmail.com>
2017-11-08 17:47:17 -0600

    [STEP] Added event handler is idempotent

--- lib/controls/task/events.rb
+++ lib/controls/task/events.rb
@@ -2,12 +2,16 @@ module Controls
   module Task
     module Events
       module Added
-        def self.example
+        def self.example(position: nil)
           added = ::Task::Messages::Events::Added.new
 
           added.task_id = Controls::ID.example
           added.name = Controls::Task::Name.example
           added.time = Controls::Time::ISO8601.example
+
+          unless position.nil?
+            added.metadata.position = position
+          end
 
           added
         end
--- lib/task/handlers/events.rb
+++ lib/task/handlers/events.rb
@@ -7,15 +7,24 @@ class Task
       include Messages::Events
 
       dependency :write, Messaging::Postgres::Write
+      dependency :get_last, MessageStore::Postgres::Get::Last
 
       def configure
         Messaging::Postgres::Write.configure(self)
+        MessageStore::Postgres::Get::Last.configure(self)
       end
 
       category :task_data
 
       handle Added do |added|
         task_id = added.task_id
+
+        stream_name = stream_name(task_id)
+
+        if current?(added)
+          Rails.logger.info { "Ignored added event (Task ID: #{task_id})" }
+          return
+        end
 
         create_command = ViewData::Commands::Create.follow(added, strict: false)
 
@@ -28,9 +37,21 @@ class Task
           :created_at => added.time
         }
 
-        stream_name = stream_name(task_id)
-
         write.(create_command, stream_name)
+      end
+
+      def current?(event)
+        task_id = event.task_id
+
+        data_command_stream = stream_name(task_id)
+
+        last_command = get_last.(data_command_stream)
+
+        return false if last_command.nil?
+
+        last_command_position = last_command.metadata[:causation_message_position]
+
+        last_command_position >= event.metadata.position
       end
     end
   end
